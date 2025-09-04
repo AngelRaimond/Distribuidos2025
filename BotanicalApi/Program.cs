5256f9a307ce276cca6e449529eabde1fda46523
Angel Raimond <angelraimond11@gmail.com>
2025-09-03 20:30:43 -0600

    Feat: Ui created

--- BotanicalApi/Program.cs
+++ BotanicalApi/Program.cs
@@ -4,16 +4,35 @@ using PlantApi.Repositories;
 using PlantApi.Services;
 using SoapCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSoapCore();
 builder.Services.AddScoped<IPlantService, PlantService>();
 builder.Services.AddScoped<IPlantRepository, PlantRepository>();
 
 builder.Services.AddDbContext<RelationalDbContext>(options =>
-options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews(); 
+builder.Services.AddRazorPages();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
 
 var app = builder.Build();
-    app.UseSoapEndpoint<IPlantService>("/PlantService.svc", new SoapEncoderOptions());
-    app.Run();
+
+app.UseStaticFiles();         
+app.UseRouting();
+app.UseCors("AllowAll");
+
+app.MapControllers();
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseSoapEndpoint<IPlantService>("/PlantService.svc", new SoapEncoderOptions());
+
+app.Run();
