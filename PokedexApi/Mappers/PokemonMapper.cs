26fa5af5c786a58e835e28b3180951d53b860bc5
RaimondMeg <angelraimond11@gmail.com>
2025-09-25 18:12:05 -0600

    feat:pokedex

--- PokedexApi/Mappers/PokemonMapper.cs
+++ PokedexApi/Mappers/PokemonMapper.cs
@@ -1,6 +1,7 @@
 using PokedexApi.Models;
 using PokedexApi.Infrastructure.Soap.Contracts;
 using PokedexApi.Dtos;
+using PokemonApi.Dtos;
 namespace PokedexApi.Mappers;
 
 public static class PokemonMapper
@@ -21,13 +22,57 @@ public static class PokemonMapper
             }
         };
     }
+
+    public static IList<Pokemon> ToModel(this IList<PokemonResponseDto> pokemonResponseDtos)
+    {
+        return pokemonResponseDtos.Select(s => s.ToModel()).ToList();
+
+    }
+
+    public static IList<PokemonResponse> ToResponse(this IList<Pokemon> pokemons)
+    {
+        return pokemons.Select(s => s.ToResponse()).ToList();
+    }
     public static PokemonResponse ToResponse(this Pokemon pokemon)
     {
         return new PokemonResponse
         {
             Id = pokemon.Id,
             Name = pokemon.Name,
+            Type = pokemon.Type,
             Attack = pokemon.stats.Attack
-        };
-    }
+        };
+    }
+
+    public static Pokemon ToModel(this CreatePokemonRequest createPokemonRequest)
+    {
+        return new Pokemon
+        {
+            Name = createPokemonRequest.Name,
+            Type = createPokemonRequest.Type,
+            Level = createPokemonRequest.Level,
+            stats = new Stats
+            {
+                Attack = createPokemonRequest.Stats.Attack,
+                Defense = createPokemonRequest.Stats.Defense,
+                Speed = createPokemonRequest.Stats.Speed
+            }
+        };
+    }
+
+    public static CreatePokemonDto ToRequest(this Pokemon pokemon) {
+        return new CreatePokemonDto
+        {
+            Name = pokemon.Name,
+            Type = pokemon.Type,
+            Level = pokemon.Level,
+            Stats = new StatsDto
+            {
+                Attack = pokemon.stats.Attack,
+                Defense = pokemon.stats.Defense,
+                Speed = pokemon.stats.Speed
+            }
+
+        };
+    }
 }
--- PokedexApi/Services/IPokemonService.cs
+++ PokedexApi/Services/IPokemonService.cs
@@ -1,8 +1,13 @@
-using PokedexApi.Models;
-
 namespace PokedexApi.Services;
 
+using PokedexApi.Models;
 public interface IPokemonService
 {
     Task<Pokemon> GetPokemonByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    Task<Pokemon> CreatePokemonAsync(Pokemon pokemon, CancellationToken cancellationToken);
+
+    Task<IList<Pokemon>> GetPokemonsAsync(string name, string type, CancellationToken cancellationToken);
+
+    Task DeletePokemonAsync(Guid id, CancellationToken cancellationToken);
 }
